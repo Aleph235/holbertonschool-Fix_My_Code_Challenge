f6b783f401076ec1acbae525323860be3bca8169
Aleph235 <akram239911@gmail.com>
2022-09-30 12:48:05 +0200

    check value

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -10,7 +10,8 @@ class Square():
     def __init__(self, *args, **kwargs):
         """doc string"""
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if value > 0:
+                setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
@@ -27,7 +28,7 @@ class Square():
 
 if __name__ == "__main__":
     """doc string"""
-    s = Square(width=12, height=9)
+    s = Square(width=-12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
