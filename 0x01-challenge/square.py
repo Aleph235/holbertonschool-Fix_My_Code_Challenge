38ec82686091e289f0b24f4b16a4dcc3401d357f
Aleph235 <akram239911@gmail.com>
2022-09-30 12:40:38 +0200

    squ

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" Documentation """
+"""doc string"""
 
 
 class Square():
-    """ Documentation """
-    size = 0
+    """doc string"""
+    width = 0
+    height = 0
 
     def __init__(self, *args, **kwargs):
-        """ Documentation """
+        """doc string"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size * self.size
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Documentation """
-        return (self.size * 2) + (self.size * 2)
+        """doc string"""
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Documentation """
-        return "{}/{}".format(self.size, self.size)
+        """doc string"""
+        return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
 
-    s = Square(size=12)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
